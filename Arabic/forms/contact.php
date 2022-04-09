9876e3e649521bb92d0e4e6f595868de7a3e083d
ShahdAbass <shahdabass94@gmail.com>
2022-04-10 02:35:53 +0300

    push

--- Arabic/forms/contact.php
+++ Arabic/forms/contact.php
@@ -14,12 +14,14 @@
   
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
+  $contact->from_phone = $_POST['phone'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
 
   
 
   $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['phone'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
